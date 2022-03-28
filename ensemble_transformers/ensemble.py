078a030d03a3b3cf3027c3ea3acdaae961cfe83c
Jake Tae <jaesungtae@gmail.com>
2022-03-28 12:47:55 -0400

    style: run linter

--- ensemble_transformers/ensemble.py
+++ ensemble_transformers/ensemble.py
@@ -2,7 +2,6 @@ from abc import abstractmethod
 
 import torch
 from torch import nn
-
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
 
@@ -18,16 +17,16 @@ class EnsembleBaseModel(nn.Module):
     def to(self, device):
         super().to(device)
         self.devices = [device for _ in range(self.num_models)]
-    
+
     def to_multiple(self, devices):
         for i, (model, device) in enumerate(zip(self.models, devices)):
             model.to(device)
             self.devices[i] = device
-    
+
     @property
     def num_models(self):
         return len(self.devices)
-    
+
     @abstractmethod
     def forward(self, *args, **kwargs):
         pass
@@ -37,16 +36,14 @@ class EnsembleModelForSequenceClassification(EnsembleBaseModel):
     def __init__(self, model_names, *args, **kwargs):
         super().__init__(model_names)
         for model_name in model_names:
-            self.models.append(
-                AutoModelForSequenceClassification.from_pretrained(model_name, *args, **kwargs)
-            )
+            self.models.append(AutoModelForSequenceClassification.from_pretrained(model_name, *args, **kwargs))
 
     def forward(
-        self, 
+        self,
         text,
-        main_device="cpu", 
-        return_all_outputs=False, 
-        tokenizer_kwargs={"return_tensors": "pt", "padding": True}
+        main_device="cpu",
+        return_all_outputs=False,
+        tokenizer_kwargs={"return_tensors": "pt", "padding": True},
     ):
         outputs = []
         for i, (model, tokenizer) in enumerate(zip(self.models, self.tokenizers)):
