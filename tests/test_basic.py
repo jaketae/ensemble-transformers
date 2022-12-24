2c26822be4cd23fdb35de69e50ccdfd650a6e601
Jake Tae <jaesungtae@gmail.com>
2022-12-24 14:29:28 +0900

    test: update test with tiny random models

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,16 +3,13 @@ import torch
 from ensemble_transformers import EnsembleModelForSequenceClassification
 
 
-class TestTextEnsemble:
-    @classmethod
-    def setup_class(cls):
-        cls.model_names = ["distilroberta-base", "xlnet-base-cased", "bert-base-uncased"]
-        cls.ensemble = EnsembleModelForSequenceClassification.from_multiple_pretrained(*cls.model_names)
-
-    def test_ensemble(self):
-        batch = ["This is a test sentence", "This is another test sentence."]
-        with torch.no_grad():
-            output = self.ensemble(batch)
-        assert len(output.logits) == len(self.model_names)
-        output.stack(self.ensemble.config.weights, "cpu")
-        assert len(output.logits) == 1
+def test_ensemble():
+    model_names = ["hf-internal-testing/tiny-bert", "hf-internal-testing/tiny-albert", "hf-internal-testing/tiny-electra", "hf-internal-testing/tiny-xlm-roberta"]
+    ensemble = EnsembleModelForSequenceClassification.from_multiple_pretrained(*model_names)
+    batch = ["This is a test sentence", "This is another test sentence."]
+    with torch.no_grad():
+        output = ensemble(batch)
+    assert len(output.logits) == len(model_names)
+    shape = output.logits[0].shape
+    output.stack(ensemble.config.weights, "cpu")
+    assert output.logits.shape == shape
--- tests/test_invalid.py
+++ tests/test_invalid.py
@@ -5,17 +5,22 @@ from ensemble_transformers.base import EnsembleBaseModel
 
 
 def test_modality():
-    pass
+    with pytest.raises(ValueError):
+        EnsembleModelForSequenceClassification.from_multiple_pretrained(
+            "hf-internal-testing/tiny-random-distilbert",
+            "hf-internal-testing/tiny-random-vit",
+        )
 
 
 def test_to_multiple():
     ensemble = EnsembleModelForSequenceClassification.from_multiple_pretrained(
-        "distilroberta-base", "bert-base-uncased"
+        "hf-internal-testing/tiny-bert",
+        "hf-internal-testing/tiny-random-distilbert",
     )
     with pytest.raises(ValueError):
         ensemble.to_multiple(["cpu"])
 
 
 def test_base_init():
-    with pytest.raises(ValueError):
-        EnsembleBaseModel.from_multiple_pretrained("bert-base-uncased")
+    with pytest.raises(RuntimeError):
+        EnsembleBaseModel.from_multiple_pretrained("hf-internal-testing/tiny-bert")
